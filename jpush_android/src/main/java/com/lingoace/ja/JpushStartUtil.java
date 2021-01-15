4ce30d629d8d67164bd389e9dc4ede23dc6ebe3b
zhaoqi <550655294@qq.com>
2021-01-15 15:06:11 +0800

    提交测试文件

--- jpush_android/src/main/java/com/lingoace/ja/JpushStartUtil.java
+++ jpush_android/src/main/java/com/lingoace/ja/JpushStartUtil.java
@@ -5,7 +5,7 @@ import android.util.Log;
 public class JpushStartUtil {
 
 
-    public  void getJpushLog(){
+    public static void getJpushLog(){
 
         Log.e("JpushStartUtil" ,"已经能获取到了Jpush相关的东西");
 
