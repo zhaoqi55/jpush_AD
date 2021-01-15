07978bb952690545d34c536d1ec8538574df36d3
zhaoqi <550655294@qq.com>
2021-01-15 14:41:35 +0800

    提交测试文件

---
+++ jpush_android/src/main/java/com/lingoace/ja/JpushStartUtil.java
@@ -0,0 +1,14 @@
+package com.lingoace.ja;
+
+import android.util.Log;
+
+public class JpushStartUtil {
+
+
+    public  void getJpushLog(){
+
+        Log.e("JpushStartUtil" ,"已经能获取到了Jpush相关的东西");
+
+    }
+
+}
---
+++ jpush_android/src/main/java/com/lingoace/ja/JpushStartView.java
@@ -0,0 +1,10 @@
+package com.lingoace.ja;
+
+public class JpushStartView {
+
+
+
+
+
+
+}
