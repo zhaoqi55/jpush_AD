009cb4bdc30434b169afb245da9dca224ffa78da
zhaoqi <qi.zhao@pplingo.com>
2021-02-23 14:25:53 +0800

    agaga

---
+++ app/src/main/java/com/zts/myapplication/GagaUtil.java
@@ -0,0 +1,17 @@
+package com.zts.myapplication;
+
+import android.util.Log;
+
+/**
+ * Created by ZhqoQi on 2021/2/23 14:22
+ * E-Mail Address：550655294@qq.com
+ */
+
+public class GagaUtil {
+
+
+    public void getUserInfo(String userName){
+
+        Log.e("=========","啊嘎嘎嘎嘎嘎嘎嘎");
+    }
+}
