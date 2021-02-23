b5a7e7fdca8c7844060378228466eb4e6eefcbc2
zhaoqi <qi.zhao@pplingo.com>
2021-02-23 16:49:09 +0800

    agaga

---
+++ common_push/src/main/java/com/pplingo/common_push/MyJPushMessageReceiver.java
@@ -0,0 +1,35 @@
+package com.pplingo.common_push;
+
+/**
+ * Created by ZhqoQi on 2021/2/23 16:47
+ * E-Mail Address：550655294@qq.com
+ */
+
+import android.content.Context;
+
+import cn.jpush.android.api.JPushMessage;
+import cn.jpush.android.service.JPushMessageReceiver;
+
+/**
+ * 自定义JPush message 接收器,包括操作tag/alias的结果返回(仅仅包含tag/alias新接口部分)
+ * */
+public class MyJPushMessageReceiver extends JPushMessageReceiver {
+
+    @Override
+    public void onTagOperatorResult(Context context, JPushMessage jPushMessage) {
+        CommonPushManager.getInstance().onTagOperatorResult(context,jPushMessage);
+        super.onTagOperatorResult(context, jPushMessage);
+    }
+    @Override
+    public void onCheckTagOperatorResult(Context context, JPushMessage jPushMessage){
+        CommonPushManager.getInstance().onCheckTagOperatorResult(context,jPushMessage);
+        super.onCheckTagOperatorResult(context, jPushMessage);
+    }
+    @Override
+    public void onAliasOperatorResult(Context context, JPushMessage jPushMessage) {
+        CommonPushManager.getInstance().onAliasOperatorResult(context,jPushMessage);
+        super.onAliasOperatorResult(context, jPushMessage);
+    }
+}
+
+
---
+++ common_push/src/main/java/com/pplingo/common_push/MyReceiver.java
@@ -0,0 +1,151 @@
+package com.pplingo.common_push;
+
+/**
+ * Created by ZhqoQi on 2021/2/23 16:44
+ * E-Mail Address：550655294@qq.com
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+
+import com.lingoace.ja.jpush.Logger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import cn.jpush.android.api.JPushInterface;
+
+/**
+ * 自定义接收器
+ *
+ * 如果不定义这个 Receiver，则：
+ * 1) 默认用户会打开主界面
+ * 2) 接收不到自定义消息
+ */
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "JPush";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        Log.d(TAG, "[MyReceiver] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+
+
+        if (JPushInterface.ACTION_REGISTRATION_ID.equals(intent.getAction())) {
+            String regId = bundle.getString(JPushInterface.EXTRA_REGISTRATION_ID);
+            Log.d(TAG, "[MyReceiver] 接收Registration Id : " + regId);
+            //send the Registration Id to your server...
+
+        } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+            Log.d(TAG, "[MyReceiver] 接收到推送下来的自定义消息: " + bundle.getString(JPushInterface.EXTRA_MESSAGE));
+//          processCustomMessage(context, bundle);
+
+        } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知");
+            int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
+            Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
+
+
+        } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {
+            Log.d(TAG, "[MyReceiver] 用户点击打开了通知");
+
+            //打开自定义的Activity
+//          Intent i = new Intent(context, TestActivity.class);
+//          i.putExtras(bundle);
+//          //i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//          i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP );
+//          context.startActivity(i);
+
+        } else if (JPushInterface.ACTION_RICHPUSH_CALLBACK.equals(intent.getAction())) {
+            Log.d(TAG, "[MyReceiver] 用户收到到RICH PUSH CALLBACK: " + bundle.getString(JPushInterface.EXTRA_EXTRA));
+            //在这里根据 JPushInterface.EXTRA_EXTRA 的内容处理代码，比如打开新的Activity， 打开一个网页等..
+
+        } else if(JPushInterface.ACTION_CONNECTION_CHANGE.equals(intent.getAction())) {
+            boolean connected = intent.getBooleanExtra(JPushInterface.EXTRA_CONNECTION_CHANGE, false);
+            Log.w(TAG, "[MyReceiver]" + intent.getAction() +" connected state change to "+connected);
+        } else {
+            Log.d(TAG, "[MyReceiver] Unhandled intent - " + intent.getAction());
+        }
+    }
+    // 打印所有的 intent extra 数据    cn.jpush.android.ALERT=ddddd
+    private static String printBundle(Bundle bundle) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_NOTIFICATION_ID)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getInt(key));
+            }else if(key.equals(JPushInterface.EXTRA_CONNECTION_CHANGE)){
+                sb.append("\nkey:" + key + ", value:" + bundle.getBoolean(key));
+            } else if (key.equals(JPushInterface.EXTRA_EXTRA)) {
+                if (bundle.getString(JPushInterface.EXTRA_EXTRA).isEmpty()) {
+                    Log.i(TAG, "This message has no Extra data");
+                    continue;
+                }
+
+                try {
+                    JSONObject json = new JSONObject(bundle.getString(JPushInterface.EXTRA_EXTRA));
+                    Iterator<String> it =  json.keys();
+
+                    while (it.hasNext()) {
+                        String myKey = it.next().toString();
+                        sb.append("\nkey:" + key + ", value: [" +
+                                myKey + " - " +json.optString(myKey) + "]");
+
+
+                    }
+                } catch (JSONException e) {
+                    Log.e(TAG, "Get message extra JSON error!");
+                }
+
+            } else {
+                sb.append("\nkey:" + key + ", value:" + bundle.getString(key));
+
+                if (key.equals("cn.jpush.android.ALERT")){
+
+                    //将推送过来的消息 保存到数据库中
+//                  PushMessageBean bean = new PushMessageBean();
+//                  bean.setContent(bundle.getString(key));
+//
+//                  ArrayList<PushMessageBean> list = new ArrayList<>();
+//                  list.add(bean);
+//                  Long insert = AppDao.getInstance().insertAll(list);
+//                  LogData.i("JPushReceiver", insert > 0 ? "保存" + insert + "条数据成功" : "保存失败");
+                    Logger.i("","Jpush消息:"+bundle.getString(key));
+                }
+
+
+            }
+        }
+        Logger.i("","Jpush消息:"+sb.toString());
+        return sb.toString();
+    }
+
+    //send msg to MainActivity
+//  private void processCustomMessage(Context context, Bundle bundle) {
+//      if (MainActivity.isForeground) {
+//          String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
+//          String extras = bundle.getString(JPushInterface.EXTRA_EXTRA);
+//          Intent msgIntent = new Intent(MainActivity.MESSAGE_RECEIVED_ACTION);
+//          msgIntent.putExtra(MainActivity.KEY_MESSAGE, message);
+//          if (!ExampleUtil.isEmpty(extras)) {
+//              try {
+//                  JSONObject extraJson = new JSONObject(extras);
+//                  if (null != extraJson && extraJson.length() > 0) {
+//                      msgIntent.putExtra(MainActivity.KEY_EXTRAS, extras);
+//                  }
+//              } catch (JSONException e) {
+//
+//              }
+//
+//          }
+//          context.sendBroadcast(msgIntent);
+//      }
+//  }
+}
+
